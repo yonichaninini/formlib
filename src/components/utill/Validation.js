75350495427318d25f950d45cb1197f1b16be898
chanmi <cksal5911@naver.com>
2019-11-07 11:44:13 +0900

    정규식 변경

--- src/components/utill/Validation.js
+++ src/components/utill/Validation.js
@@ -1,6 +1,6 @@
 const Validation = {
   isEmail: email => {
-    let validationEmail = /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i; //이메일
+    let validationEmail = /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/i; //이메일
     return validationEmail.test(email);
   },
   isPhoneNumber: phoneNumber => {
